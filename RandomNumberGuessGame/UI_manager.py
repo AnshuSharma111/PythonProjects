e2a41e9d4bd53a1999f7a43c43d311b96156d363
Keyboard-1 <anshu045sharma@gmail.com>
2024-10-03 22:56:54 +0530

    Final Draft Ready

--- RandomNumberGuessGame/UI_manager.py
+++ RandomNumberGuessGame/UI_manager.py
@@ -2,11 +2,24 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from tkinter import messagebox
 import random
+import pickle
+import os
+
+basedir = os.path.dirname(__file__)
+DATA_PATH = os.path.join(basedir, 'data.pickle')
 
 frame = None
-def initialise(root):
+data = None
+
+def initialise_app(root):
     global frame
+    global data
     frame = root
+
+    with open(DATA_PATH, 'rb') as f:
+        data = pickle.load(f)
+        print(data)
+
     homeScreen()
 
 def clearFrame():
@@ -34,16 +47,36 @@ def submitProceed(l, u, t):
     options = {"lower" : lr, "upper" : ur, "tries" : tr, "randomNumber" : random.randrange(lr, ur)}
     playScreen(options)
 
-def gameEnd(outcome, randNum):
-    if not outcome:
-        messagebox.showinfo("Lose", f"Game Over! The number was {randNum}")
+def updateStats(outcome, options):
+    data['totalPlayed'] += 1
+
+    #update data
+    if outcome[0]:
+        data['totalWon'] += 1
+        data['streak'] += 1
+        # calculate score
+        score = round(((options['upper'] - options['lower']) / outcome[1]), 2)
+        print(score)
+        if score > data['highestScore']:
+            data['highestScore'] = score
+    else:
+        data['streak'] = 0
+    
+    data['winRate'] = round(((data['totalWon']/data['totalPlayed']) * 100), 2)
+    with open(DATA_PATH, 'wb') as f:
+        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+
+def gameEnd(outcome, options):
+    if not outcome[0]:
+        messagebox.showinfo("Lose", f"Game Over! The number was {options['randomNumber']}")
     else:
         messagebox.showinfo("Won", f"Congratulations! You win!")
-    
+
+    updateStats(outcome, options)
     homeScreen()
     
 
-def check(userInp, actualVal, outputLabel, triesLeft):
+def check(userInp, options, outputLabel, triesLeft):
     try:
         userInp = int(userInp)
         tl = int(triesLeft.cget("text")[13:])
@@ -51,13 +84,13 @@ def check(userInp, actualVal, outputLabel, triesLeft):
 
         triesLeft.config(text=f"Tries Left : {max(0,tl-1)}")
         if tl > 0:
-            if userInp == actualVal:
+            if userInp == options["randomNumber"]:
                 outputLabel.config(text="Congratulations! You guessed right!")
                 won = True
                 triesLeft.config(text=f"Tries Left : {tl-1}")
                 tl -= 1
-                gameEnd(won, actualVal)
-            elif userInp < actualVal:
+                gameEnd([won, options['tries'] - tl] , options)
+            elif userInp < options["randomNumber"]:
                 outputLabel.config(text="You guessed lesser than the actual Number!")
             else:
                 outputLabel.config(text="You guessed higher than the actual Number!")
@@ -65,7 +98,7 @@ def check(userInp, actualVal, outputLabel, triesLeft):
             if tl == 1 and not won:
                 outputLabel.config(text="Congratulations! You guessed right!")
                 tl -= 1
-                gameEnd(False, actualVal)
+                gameEnd([won, options['tries'] - tl], options)
         else:
             outputLabel.config(text = "Out of tries!")
     except:
@@ -81,6 +114,21 @@ def homeScreen():
     # Play Button
     playButton = ttk.Button(frame, bootstyle="dark", text="Play!", command= lambda : optionsScreen())
     playButton.pack(pady=50)
+    # Stats Frame
+    statsFrame = ttk.Frame(frame)
+    statsFrame.pack()
+    # Streak
+    streak = ttk.Label(statsFrame, text = f"Win Streak : {data['streak']}", font=("Helvetica", 15))
+    streak.pack(side=LEFT)
+    # Total Games
+    games = ttk.Label(statsFrame, text = f"Games Played : {data['totalPlayed']}", font=("Helvetica", 15))
+    games.pack(side=LEFT, padx=20)
+    # Highest Score
+    highScore = ttk.Label(statsFrame, text = f"Highest Score : {data['highestScore']}", font=("Helvetica", 15))
+    highScore.pack(side=LEFT, padx=20)
+    # Win rate
+    winRate = ttk.Label(statsFrame, text = f"Win Rate: {data['winRate']}", font=("Helvetica", 15))
+    winRate.pack(side=LEFT, padx=20)
 
 def optionsScreen():
     clearFrame()
@@ -138,5 +186,5 @@ def playScreen(options):
     output.pack(pady=10)
 
     # check button
-    checkButton = ttk.Button(frame, bootstyle="dark", text="Proceed", command= lambda : check(inputField.get(), options['randomNumber'], output, triesLeft))
+    checkButton = ttk.Button(frame, bootstyle="dark", text="Proceed", command= lambda : check(inputField.get(), options, output, triesLeft))
     checkButton.pack(pady=50)
--- RandomNumberGuessGame/app.py
+++ RandomNumberGuessGame/app.py
@@ -1,17 +1,19 @@
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from UI_manager import *
+import os
 
 root = ttk.Window(themename="cyborg")
 
+basedir = os.path.dirname(__file__)
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 TITLE = "Random Number Guessing Game"
-ICON_PATH = 'RandomNumberGuessGame\icon_1.ico'
+ICON_PATH = os.path.join(basedir, 'icon.ico')
 
 root.iconbitmap(ICON_PATH)
 root.title(TITLE)
 root.geometry(f"{screen_width}x{screen_height}")
 
-initialise(root)
+initialise_app(root)
 root.mainloop()
